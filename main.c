fef7223233a1ab56cbc32fbd46a7c7f915ca8c05
Alexander Rødseth <rodseth@gmail.com>
2012-12-14 14:15:23 +0100

    The beginnings of drawing a torus

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* vim:set ts=2 sw=2 et: */
 /*
- * Noisy Triangle
+ * Concert Master
  * A skeleton for 4k intros for Linux
  *
  * Alexander Rødseth <rodseth@gmail.com>
@@ -8,9 +8,11 @@
  *
  */
 
-#include "GL/gl.h"
-#include "SDL/SDL.h"
-#include "alsa/asoundlib.h"
+#include <GL/gl.h>
+#include <SDL/SDL.h>
+#include <alsa/asoundlib.h>
+#include <stdio.h>
+#include <math.h>
 
 /* Assembly functions */
 extern int add(int a, int b);
@@ -20,33 +22,64 @@ extern void quit();
 static char *device = "default";
 snd_output_t *output = NULL;
 unsigned char buffer[16*1024];
-const char *audio_error = "sound fail";
+
+static float PI = 3.14;
+
+float skew = 0.0;
+
+void torus()
+{
+  float r = 0.0;
+  float x = sin(r+skew) * 2.0f;
+  float y = cos(r+skew) * 2.0f;
+  float prevx = x;
+  float prevy = y;
+  while (r < (2.0f*PI))
+  {
+    x = sin(r+skew) * 2.0f;
+    y = cos(r+skew) * 2.0f;
+
+    printf("%f %f %f\n", x, y, r);
+
+    glBegin(GL_TRIANGLES);
+    glVertex3i( 0.0, 0.0, -10);
+    glVertex3i( prevx, prevy, -10);
+    glVertex3i( x,  y, -10);
+    glEnd();
+
+    r += 0.1;
+    prevx = x;
+    prevy = y;
+  }
+  skew += 0.01;
+  printf(".");
+
+}
 
 void _start()
 {
-  unsigned int i;
-  int err;
   SDL_Event event;
+  
+  int err;
+  unsigned int i;
   snd_pcm_t *handle;
   snd_pcm_sframes_t frames;
 
-  for (i = 0; i < sizeof(buffer); i++)
+  for (i=0; i<sizeof(buffer); i++)
   {
     /* Generate sound here */
-    buffer[i] = random() & 0xff;
+    buffer[i] = i ^ i; //random() & 0xff;
   }
   if ((err=snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0)
   {
-    //printf("Playback open error: %s\n", snd_strerror(err));
-    printf("%s\n", audio_error);
+    printf("Playback open error: %s\n", snd_strerror(err));
     exit(EXIT_FAILURE);
   }
   if ((err=snd_pcm_set_params(handle,SND_PCM_FORMAT_U8,
                              SND_PCM_ACCESS_RW_INTERLEAVED,
                              1, 48000, 1, 500000)) < 0)
   {
-    //printf("Playback open error: %s\n", snd_strerror(err));
-    printf("%s\n", audio_error);
+    printf("Playback open error: %s\n", snd_strerror(err));
     exit(EXIT_FAILURE);
   }
 
@@ -61,7 +94,7 @@ void _start()
   glShadeModel(GL_FLAT);
   
   glLoadIdentity();
-  glColor3f(1.0, 1.0, 0.0);
+  glColor3f(0.0, 0.0, 1.0);
   glBegin(GL_TRIANGLES);
   glVertex3i( 1,  1, -10);
   glVertex3i( 1, -1, -10);
@@ -75,14 +108,19 @@ void _start()
       frames = snd_pcm_recover(handle, frames, 0);
     }
     if (frames < 0) {
-      //printf("snd_pcm_writei failed: %s\n", snd_strerror(err));
-      printf("%s\n", audio_error);
+      printf("snd_pcm_writei failed: %s\n", snd_strerror(err));
       break;
     }
-    if (frames > 0 && frames < (long)sizeof(buffer)) {
-      //printf("Short write (expected %li, wrote %li)\n", (long)sizeof(buffer), frames);
-      printf("%s\n", audio_error);
+    if(frames > 0 && frames < (long)sizeof(buffer)) {
+      printf("Short write (expected %li, wrote %li)\n",
+          (long)sizeof(buffer), frames);
     }
+
+    glLoadIdentity();
+    glColor3f(1.0, 0.0, 0.0);
+    torus();
+    SDL_GL_SwapBuffers();
+
     SDL_PollEvent(&event);
   } while (event.type != SDL_KEYDOWN);
 
